ee6eb46990bd3bb38d3c1e0438865c58811e1588
bamlakugetachew1 <101706961+bamlakugetachew1@users.noreply.github.com>
2022-12-02 21:16:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const bodyparser = require("body-parser");
-const connection = require("./models/connection");
-const messagescontrollers = require("./controllers/messagecontroller");
 const app = express();
 app.use(cors());
 app.use(bodyparser.urlencoded({
@@ -18,7 +16,25 @@ app.use(morgan("dev"));
 // app.use('/messages',messagesmodelcontrollers);
 app.use('/messages',messagescontrollers);
 
-app.listen(process.env.PORT || 5000, () => {
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.Mongourl);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+connectDB().then(() => {
+  app.listen(process.env.PORT || 5000, () => {
   console.log("app listing on port 3000 if port 5000 is free");
 });
+})
+
+const connection = require("./models/connection");
+const messagescontrollers = require("./controllers/messagecontroller");
+
+
+
 
