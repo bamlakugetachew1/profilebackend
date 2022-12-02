7aabfc44f42644f55718cde0c2fae762a69fb2ce
bamlakugetachew1 <101706961+bamlakugetachew1@users.noreply.github.com>
2022-12-02 21:16:54 +0300

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.Mongourl,(error)=>{ 
- if(!error){
-    console.log('database is connecetd');
- }
-});
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.Mongourl,(error)=>{ 
+//  if(!error){
+//     console.log('database is connecetd');
+//  }
+// });
 
 const messages = require('./message.model')
