5070830530ebe74359b4b9fced7d2983fe1c8f7b
bamlakugetachew1 <101706961+bamlakugetachew1@users.noreply.github.com>
2022-12-02 21:34:02 +0300

    Update messagecontroller.js

--- controllers/messagecontroller.js
+++ controllers/messagecontroller.js
@@ -30,19 +30,21 @@ router.post("/addmessages", async (req, res) => {
   });
   await messages
     .save()
-    .then(() => {
-
-      res.json({
-        message: 1,
-      });
-
-      transporter.sendMail(mailOptions, function(error, info){
+    .then((data) => {
+        transporter.sendMail(mailOptions, function(error, info){
         if (error) {
           console.log(error);
         } else {
           console.log('Email sent: ' + info.response);
         }
       });
+      
+      res.json({
+        message: 1,
+        data:data
+      });
+
+  
 
 
     })
